96d34260c5a8417896db7d3c99d5047debe44c51
yuki <yukihiguchi0826@gmail.com>
2024-08-06 19:58:50 -0700

    make stage until level 14

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -4,16 +4,25 @@ import Data from "../../../public/stages.json"
 
 export const Menu = () => {
   return (
-    <div>
-      <h1>menu</h1>
-      {
-        Data.map((stage) => (
-          <div>
-            <h1>Level {stage.id}</h1>
-            <p>{stage.letters.map((letter) => letter)}</p>
-          </div>
-        ))
-      }
+    <div className="py-10 flex justify-center">
+      <div className="max-w-5xl">
+        <div className="grid place-items-center grid-cols-3 gap-y-7 gap-x-14">
+          {
+            Data.map((stage) => (
+              <div key={stage.id} className="bg-slate-50 rounded-md w-60 h-40">
+                <a href={`/quiz/${stage.id}`} className="h-full grid place-items-center">
+                  <div>
+                    <h1 className="text-3xl font-bold">Level {stage.id}</h1>
+                    <p className="text-2xl">{stage.letters.map((letter) => letter)}</p>
+                  </div>
+                </a>
+              </div>
+            ))
+          }
+        </div>
+
+      </div>
+
     </div>
   );
 };
