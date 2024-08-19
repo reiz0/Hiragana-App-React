5cae1efe4c9e0b6e874217b13c9e8298268aa4e1
yuki <yukihiguchi0826@gmail.com>
2024-08-19 01:17:48 -0700

    make responsive design and set appname and icon

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -6,7 +6,7 @@ export const Menu = () => {
   return (
     <div className="py-10 flex justify-center">
       <div className="max-w-5xl">
-        <div className="grid place-items-center grid-cols-3 gap-y-7 gap-x-14">
+        <div className="grid place-items-center grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-y-7 gap-x-14">
           {
             Data.map((stage) => (
               <div key={stage.id} className="bg-slate-50 rounded-md w-60 h-40">
